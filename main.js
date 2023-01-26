0191dc14a5f436c59a86120a48638d8c583c4fdb
ShrihanJerry <122576003+ShrihanJerry@users.noreply.github.com>
2023-01-26 17:49:18 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+console.log("Hello")
+var first_name="Shrihan"
+var second_name="Jared"
+var full_name= first_name.concat(second_name)
+console.log(full_name)
+var word="I am Shrihan Jared"
+var replaced=word.replace("Shrihan","Bob")
+console.log(word)
+console.log(replaced)
+var name1="shrihan"
+var upper=name1.toUpperCase()
+console.log(name1)
+var name2="JARED"
+var lower=name2.toLowerCase()
+console.log(name2)
+var number= Math.sqrt(123)
+console.log(number)
+var number1= Math.max(1,3)
+console.log(number1)
+var number2=Math.min(1,10)
+console.log(number2)
+var powers=Math.pow(9,10)
+console.log(powers)
+console.log("My first console project :)")
+var fname="Dog"
+var lname="Cat"
+var allname= fname.concat(lname)
+console.log(allname)
+var a=5
+var b=10
+console.log(a+b)
+console.log(b-a)
+console.log(a*b)
+console.log(b/a)
